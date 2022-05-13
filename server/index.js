040bd670ce9856b5a4822c3d429493caafe8d780
Nikolaj Mertz <nikolajmertz@gmail.com>
2022-05-13 22:23:44 +0200

    MVP GraphQL photocube server + benchmark results

---
+++ server/index.js
@@ -0,0 +1,99 @@
+const { gql, ApolloServer } = require("apollo-server");
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+const neo4j = require("neo4j-driver");
+require("dotenv").config();
+
+const typeDefs = gql`
+  type Object {
+    id: Int @unique
+    file_type: Int
+    file_uri: String
+    thumbnail_uri: String
+    tags: [TagType!]! @relationship(type: "TAGGED", direction: OUT)
+  }
+
+  interface Tag {
+    id: Int 
+    objects: [Object!]! @relationship(type: "TAGGED", direction: IN)
+    tagset: [Tagset!]! @relationship(type: "IN_TAGSET", direction: OUT)
+    node: [HierarchyNode!]! @relationship(type: "REPRESENTS", direction: IN)
+  }
+
+  union TagType = AlphanumericalTag | NumericalTag | DateTag | TimeTag | TimestampTag
+
+  type AlphanumericalTag implements Tag @node(label: "Alphanumerical", additionalLabels: ["Tag"]) {
+    id: Int @unique
+    name: String @alias(property: "name")
+    objects: [Object!]! @relationship(type: "TAGGED", direction: IN)
+    tagset: [Tagset!]! @relationship(type: "IN_TAGSET", direction: OUT)
+    node: [HierarchyNode!]! @relationship(type: "REPRESENTS", direction: IN)
+  }
+
+  type NumericalTag implements Tag @node(label: "Numerical", additionalLabels: ["Tag"]) {
+    id: Int @unique
+    value: Int @alias(property: "name")
+    objects: [Object!]! @relationship(type: "TAGGED", direction: IN)
+    tagset: [Tagset!]! @relationship(type: "IN_TAGSET", direction: OUT)
+    node: [HierarchyNode!]! @relationship(type: "REPRESENTS", direction: IN)
+  }
+
+  type DateTag implements Tag @node(label: "Date", additionalLabels: ["Tag"]) {
+    id: Int @unique
+    date: Date @alias(property: "name")
+    objects: [Object!]! @relationship(type: "TAGGED", direction: IN)
+    tagset: [Tagset!]! @relationship(type: "IN_TAGSET", direction: OUT)
+    node: [HierarchyNode!]! @relationship(type: "REPRESENTS", direction: IN)
+  }
+
+  type TimeTag implements Tag @node(label: "Time", additionalLabels: ["Tag"]) {
+    id: Int @unique
+    time: Time @alias(property: "name")
+    objects: [Object!]! @relationship(type: "TAGGED", direction: IN)
+    tagset: [Tagset!]! @relationship(type: "IN_TAGSET", direction: OUT)
+    node: [HierarchyNode!]! @relationship(type: "REPRESENTS", direction: IN)
+  }
+
+  type TimestampTag implements Tag @node(label: "Timestamp", additionalLabels: ["Tag"]) {
+    id: Int @unique
+    timestamp: Duration @alias(property: "name")
+    objects: [Object!]! @relationship(type: "TAGGED", direction: IN)
+    tagset: [Tagset!]! @relationship(type: "IN_TAGSET", direction: OUT)
+    node: [HierarchyNode!]! @relationship(type: "REPRESENTS", direction: IN)
+  }
+
+  type Tagset {
+    id: Int @unique
+    name: String
+    tags: [TagType!]! @relationship(type: "HAS_TAG", direction: OUT)
+  }
+
+  type HierarchyNode @node(label: "Node") {
+    id: Int @unique
+    parent: HierarchyNode @relationship(type: "HAS_PARENT", direction: OUT)
+    hierarchy: Hierarchy @relationship(type: "IN_HIERARCHY", direction: OUT)
+    represents: Tag @relationship(type: "REPRESENTS", direction: OUT)
+  }
+
+  type Hierarchy {
+    id: Int @unique
+    name: String
+    root: HierarchyNode @relationship(type: "HAS_ROOT", direction: OUT)
+  }
+`;
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+neoSchema.getSchema().then((schema) => {
+    const server = new ApolloServer({
+        schema: schema
+    });
+
+    server.listen().then(({ url }) => {
+        console.log(`GraphQL server ready on ${url}`);
+    });
+});
