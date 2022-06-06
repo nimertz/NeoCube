9bd484fe8424be0fc70a449953a09b6350cc8500
Nikolaj Mertz <nikolajmertz@gmail.com>
2022-06-06 17:29:56 +0200

    Rewrote custom resolver for cell query

--- server/index.js
+++ server/index.js
@@ -87,8 +87,24 @@ const typeDefs = gql`
     root: HierarchyNode @relationship(type: "HAS_ROOT", direction: OUT)
   }
 
+  type Cell {
+    idx: Int
+    idy: Int
+    idz: Int
+    file_uri: String
+    cnt: Int
+  }
+
+  enum FilterType {
+    H
+    S
+    T
+  }
+
+
   type Query {
-    cell(TagIDs: [Int], NodeIDs: [Int]) : [Object!]! @cypher(statement: "MATCH (tag : Tag) WHERE tag.id IN $TagIDs WITH collect(tag) as tags WITH tags MATCH (root : Node) WHERE root.id IN $NodeIDs WITH tags, collect(root) as nodes MATCH (o) WHERE ALL(r IN nodes WHERE (r)<-[:HAS_PARENT*0..]-(:Node)-[:REPRESENTS]->(: Tag)<-[:TAGGED]-(o) ) AND ALL(t IN tags WHERE (o)-[:TAGGED]->(t)) RETURN o")
+    cell(Dimensions: Int, FilterTypes: [FilterType], FilterIDs: [Int]) : [Object!]! 
+    state(Dimensions: Int, FilterTypes: [FilterType], FilterIDs: [Int]) : [Cell!]!
   }
 `;
 const loggingConfig = { logging: neo4j.logging.console('debug') };
@@ -100,10 +116,81 @@ const driver = neo4j.driver(
 );
 
 const ogm = new OGM({ typeDefs, driver });
-const neoObjects = ogm.model("Object");
+//const neoObjects = ogm.model("Object");
 
 const resolvers = {
+  Query: {
+    cell: async (root, args) => {
+      const { Dimensions, FilterTypes, FilterIDs } = args;
+      
 
+      if (Dimensions == 0 | FilterTypes == null) {
+        query_all = "MATCH(o: Object) RETURN o.id as Id, o.file_uri as fileURI;";
+        const result = await driver.session().readTransaction(tx => {
+          return tx.run(query_all);
+        }
+        ).then(result => {
+          return result.records.map(record => {
+            return record.get("o").properties;
+          }
+          );
+        }
+        ).catch(error => {
+          console.log(error);
+        }
+        );
+        return result;
+      } else {
+        console.log("Dimensions: " + Dimensions);
+        var frontstr = ""  // add profile / explain here
+        var midstr = ""
+        var endstr = "RETURN DISTINCT o;"
+
+        for (let i = 0; i < Dimensions; i++) {
+          var curr = i + 1;
+          if (FilterTypes[i] == "T") {
+            midstr += `MATCH (dim${curr}_t: Tag {id: ${FilterIDs[i]}})\n`;
+            midstr += `MATCH (dim${curr}_t: Tag)<-[:TAGGED]-(o: Object)\n`;
+          } else if (FilterTypes[i] == "H") {
+            midstr += `MATCH (dim${curr}_n: Node {id: ${FilterIDs[i]}})\n`;
+            midstr += `MATCH (dim${curr}_n)<-[:HAS_PARENT]-(R${curr} : Node)<-[:HAS_PARENT*0..]-(: Node)-[:REPRESENTS]->(: Tag)<-[:TAGGED]-(o: Object)\n`;
+          }
+        }
+
+        const numtots = FilterIDs.length;
+        for (let i = Dimensions; i < numtots; i++) {
+          var cur = i + 1;
+          if(FilterTypes[i] == "H") {
+            midstr += `MATCH (fil${cur}_n: Node {id: ${FilterIDs[i]}})\n`; 
+            midstr += `MATCH (fil${cur}_n)<-[:HAS_PARENT]-(R${curr} : Node)<-[:HAS_PARENT*]-(: Node)-[:REPRESENTS]->(: Tag)<-[:TAGGED]-(o: Object)\n`;
+          } else if(FilterTypes[i] == "T") {
+            midstr += `MATCH (fil${cur}_t: Tag {id: ${FilterIDs[i]}})\n`;
+            midstr += `MATCH (fil${cur}_t: Tag)<-[:TAGGED]-(o: Object)\n`;
+          } else if(FilterTypes[i] == "S") {
+            midstr += `MATCH (fil${cur}_t: Tagset {id: ${FilterIDs[i]}})\n`;
+            midstr += `MATCH (fil${cur}_t)<-[:IN_TAGSET]-(:Tag)<-[:TAGGED]-(o: Object)\n`;
+          }
+        }
+
+        const neo4j_cell_query = frontstr + midstr + endstr;
+
+        const result = await driver.session().readTransaction(tx => {
+          return tx.run(neo4j_cell_query);
+        }
+        ).then(result => {
+          return result.records.map(record => {
+            return record.get("o").properties;
+          }
+          );
+        }
+        ).catch(error => {
+          console.log(error);
+        }
+        );
+        return result;
+      }
+    }
+  }
 };
 
 
